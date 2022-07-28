513c50d6b2969d0c7e024beab5075acfab940301
rdenise <remi.denise@gmail.com>
2022-07-28 18:34:42 +0100

    :bug: problem with lst now

--- create_ictv.py
+++ create_ictv.py
@@ -374,6 +374,9 @@ def gbk2lst(replicon, lst_file) :
         note = ' '.join(sequence.qualifiers['note'])
         tmp_dict['product_note'].append(f'|{product}|{note}')
 
+        logging.debug(f"Protein: {sequence.qualifiers['translation'][0]}")
+        logging.debug(f"Gene: {sequence.extract(replicon).seq}")
+
         tmp_dict['sequence_aa'].append(sequence.qualifiers['translation'][0])
         tmp_dict['sequence_nt'].append(sequence.extract(replicon).seq)
 
